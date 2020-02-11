cc80bd005b274f4788dd0e0fd0a4f366737215d5
CHANUT jean-baptiste <jbchanut@hotmail.com>
2020-02-11 10:04:06 +0100

    Quete2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
+import Contact from "./Contact";
 
 function App() {
-
-
-    const name = "Pierre"
-    const avatar = "https://scontent-cdg2-1.xx.fbcdn.net/v/t31.0-8/s960x960/10258663_1381675435449935_3169864455524463304_o.jpg?_nc_cat=108&_nc_oc=AQlf-RE8yVmxRKVOAh4Z313QADRXhHMN-MqCi3W5c2GKoZN2w8U9tT15jvD_DWhPWnkD6hmahtrVm9qvbGoAeuvh&_nc_ht=scontent-cdg2-1.xx&oh=0ccd66900168ce8df7b3c8f75af0fdd8&oe=5ED0AF73"
-    const online = false
 
   return (
     <div className="App">
       <header className="App-header">
           <ul className="list-group w-50">
-              <li className="list-group-item d-flex justify-content-between align-items-center">
-                  <img src={avatar} className="profil-img rounded-circle" alt="pierre"></img>
-                  <div className="text-dark">{name}</div>
-                  <div className={online ? "cercle bg-success" : "cercle bg-danger"}> </div>
-              </li>
+              <Contact />
           </ul>
       </header>
     </div>
